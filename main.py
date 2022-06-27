1ce3cccc801820a7ec80e368a8368057399e2363
teisnp <teis.n.p@gmail.com>
2022-06-27 14:34:08 +0200

    added example load of detla table to main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """ Entry point"""
 
 import PySimpleGUI as sg
+from storage import storage_service
+from deltalake import PyDeltaTableError
+
+id = "123"
+
+try:
+    table = storage_service.get_data(id)
+except PyDeltaTableError as err:
+    print(err)
+    exit()
 
 sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
